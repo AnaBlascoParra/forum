9d522f3152d3066dc33c81ee1c4c4d7f48a56743
Ana <blasco.paana21@cadiz.salesianos.edu>
2022-09-27 08:26:25 +0200

    first commit

---
+++ app/Forum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    protected $table='forums';
+
+    protected $fillable=['id','name'];
+
+    protected $hidden=
+}
